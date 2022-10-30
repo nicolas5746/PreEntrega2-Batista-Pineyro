53d2bc5a2b85d8bf613dae088ecb2d5ba5a178f3
Nicolas Batista Piñeyro <nicolas26386@gmail.com>
2022-10-30 20:24:07 -0300

    v1.15

--- src/screens/cart/Cart.jsx
+++ src/screens/cart/Cart.jsx
@@ -1,7 +1,13 @@
+import React from 'react';
+import { CartContext } from 'contexts/cart/CartContext';
 import './Cart.sass';
 
 const Cart = () => {
-
+  return (
+    <div className="cart">
+      <CartContext.Provider></CartContext.Provider>
+    </div>
+  );
 }
 
 export default Cart;
